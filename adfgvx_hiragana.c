bc406a46295279d10c8808d7e770b1394d5b10ba
piry9200 <ymt104ro09@gmail.com>
2022-07-10 17:54:17 +0900

    プロトタイプ完成。ひらがなを入力すると、換字暗号にしてくれる

--- adfgvx_hiragana.c
+++ adfgvx_hiragana.c
@@ -5,7 +5,7 @@
 
 int main(int argc, const char * argv[]){
     setlocale(LC_CTYPE, ""); //ワイド文字列を出力するために必要
-    wchar_t key[] = {L'き',L'さ',L'ま',L'み',L'て',L'い',L'る',L'な',L'!'};
+    wchar_t key[9] = {L'き',L'さ',L'ま',L'み',L'て',L'い',L'る',L'な',L'!'};
     wchar_t table[9][9] ={
             {L'あ',L'い',L'う',L'え',L'お',L'か',L'き',L'く',L'け'},
             {L'こ',L'さ',L'し',L'す',L'せ',L'そ',L'た',L'ち',L'つ'},
@@ -18,10 +18,10 @@ int main(int argc, const char * argv[]){
             {L'1',L'2',L'3',L'4',L'5',L'6',L'7',L'8',L'9'}
         };
 
-    wchar_t scan[501] = {L"あいうえお"};
+    wchar_t scan[501] = {L'\0'};
     int flag_1 = 0;
 
-    //scanf("%ls",scan);
+    scanf("%ls",scan);
     for(int i = 0; i < sizeof(scan); i++){ //scanの文字をひとつずつチェック
 
         if(scan[i] == '\0'){ //scanから取り出した値が'\0'だったらscanの文字列を参照するのを中断
@@ -29,10 +29,10 @@ int main(int argc, const char * argv[]){
         break;
         }
 
-        for(int r = 0; r < 6; r++){  //scanから取り出した文字が表の何行何列にあるかを確かめる r=行 c=列
-            for(int c = 0; c < 6; c++){
+        for(int r = 0; r < 9; r++){  //scanから取り出した文字が表の何行何列にあるかを確かめる r=行 c=列
+            for(int c = 0; c < 9; c++){
                 if(scan[i] == table[r][c]){
-                    wprintf(L"%lc",table[r][c]);
+                    wprintf(L"%lc%lc", key[r], key[c]);
                     flag_1 = 1; //表から文字が見つかったらそれを出力してflagを立てる
                     break; //列のインクリメントを中断
                 }
